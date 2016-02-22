7193c22941956baf608e978e133c5ca3cc3eef79
Teemu Sarapisto <tsarpf@gmail.com>
2016-02-22 22:41:55 +0200

    matching 'done' but not working

--- src/bejeweled_bot/core.clj
+++ src/bejeweled_bot/core.clj
@@ -1,15 +1,37 @@
 (ns bejeweled-bot.core
   (:import java.awt.Robot)
   (:import java.awt.event.InputEvent)
-  (:import '[org.opencv.core Mat Size Highgui])
-  (:import '[org.opencv.highgui Highgui])
   (:gen-class))
+
+
+(clojure.lang.RT/loadLibrary org.opencv.core.Core/NATIVE_LIBRARY_NAME)
+(import '[org.opencv.core Mat Size CvType]
+        '[org.opencv.highgui Highgui]
+        '[org.opencv.imgproc Imgproc])
+
+;byte[] data = ((DataBufferByte) image.getRaster().getDataBuffer()).getData();
+;Mat mat = new Mat(width, height, CvType.CV_8UC3);
+;mat.put(0, 0, data);
 
 (defn -main
   "I'm a doc string"
   [& args]
-  (def sourceimg (Highgui/imread "resources/area.png"))
+  ;(def derp (Mat. 512 512 ns/CvType/CV_8UC3))
+  (def sourceImg (Highgui/imread "resources/area.png"))
   (def diamond (Highgui/imread "resources/diamond.png"))
+  (Highgui/imwrite "resources/output.png" diamond)
+  (def cols (.cols sourceImg))
+  (def rows (.rows sourceImg))
+  (def result (Mat.))
+  (.create result rows, cols, CvType/CV_32FC1)
+  (def displayImg (Mat.))
+  (.copyTo sourceImg displayImg)
+  (Imgproc/matchTemplate sourceImg diamond result Imgproc/TM_CCOEFF_NORMED)
+  (def threshold 0.8)
+
+  ;  normalize( result, result, 0, 1, NORM_MINMAX, -1, Mat() );
+  ;(Highgui/imwrite "resources/output.png" result)
+
   (let [robo (Robot.)]
     (.mousePress robo InputEvent/BUTTON1_MASK)
     (.mouseMove robo 1600 100)
