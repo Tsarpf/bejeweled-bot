33677031b3636dbbee7af7533d27f28ba911b2ff
Teemu Sarapisto <tsarpf@gmail.com>
2016-02-22 23:15:13 +0200

    it werks!j

--- src/bejeweled_bot/core.clj
+++ src/bejeweled_bot/core.clj
@@ -5,7 +5,7 @@
 
 
 (clojure.lang.RT/loadLibrary org.opencv.core.Core/NATIVE_LIBRARY_NAME)
-(import '[org.opencv.core Mat Size CvType]
+(import '[org.opencv.core Mat Size CvType Core]
         '[org.opencv.highgui Highgui]
         '[org.opencv.imgproc Imgproc])
 
@@ -16,26 +16,25 @@
 (defn -main
   "I'm a doc string"
   [& args]
-  ;(def derp (Mat. 512 512 ns/CvType/CV_8UC3))
   (def sourceImg (Highgui/imread "resources/area.png"))
   (def diamond (Highgui/imread "resources/diamond.png"))
-  (Highgui/imwrite "resources/output.png" diamond)
-  (def cols (.cols sourceImg))
-  (def rows (.rows sourceImg))
+  (def cols (+ (- (.cols sourceImg) (.cols diamond)) 1))
+  (def rows (+ (- (.rows sourceImg) (.rows diamond)) 1))
   (def result (Mat.))
   (.create result rows, cols, CvType/CV_32FC1)
   (def displayImg (Mat.))
   (.copyTo sourceImg displayImg)
   (Imgproc/matchTemplate sourceImg diamond result Imgproc/TM_CCOEFF_NORMED)
+  ;(Imgproc/matchTemplate sourceImg diamond result Imgproc/TM_CCORR_NORMED)
   (def threshold 0.8)
 
-  ;  normalize( result, result, 0, 1, NORM_MINMAX, -1, Mat() );
-  ;(Highgui/imwrite "resources/output.png" result)
+  (Core/normalize result result 0 255 Core/NORM_MINMAX -1 (Mat.))
+  (Highgui/imwrite "resources/output.png" result)
 
-  (let [robo (Robot.)]
-    (.mousePress robo InputEvent/BUTTON1_MASK)
-    (.mouseMove robo 1600 100)
-    (.mouseRelease robo InputEvent/BUTTON1_MASK)
-    ))
+  ;(let [robo (Robot.)]
+  ;  (.mousePress robo InputEvent/BUTTON1_MASK)
+  ;  (.mouseMove robo 1600 100)
+  ;  (.mouseRelease robo InputEvent/BUTTON1_MASK)
+  ;  ))
 
 (-main)
