7cc7fab54243be15a7157eb0dce18ae9b0b13644
Teemu Sarapisto <tsarpf@gmail.com>
2016-02-23 03:07:52 +0200

    Searching for game area works!

--- src/bejeweled_bot/core.clj
+++ src/bejeweled_bot/core.clj
@@ -5,7 +5,7 @@
 
 
 (clojure.lang.RT/loadLibrary org.opencv.core.Core/NATIVE_LIBRARY_NAME)
-(import '[org.opencv.core Mat Size CvType Core]
+(import '[org.opencv.core Mat Size CvType Core Point Scalar]
         '[org.opencv.highgui Highgui]
         '[org.opencv.imgproc Imgproc])
 
@@ -16,10 +16,12 @@
 (defn -main
   "I'm a doc string"
   [& args]
-  (def sourceImg (Highgui/imread "resources/area.png"))
-  (def diamond (Highgui/imread "resources/diamond.png"))
-  (def cols (+ (- (.cols sourceImg) (.cols diamond)) 1))
-  (def rows (+ (- (.rows sourceImg) (.rows diamond)) 1))
+  (def sourceImg (Highgui/imread "resources/whole.png"))
+  (def diamond (Highgui/imread "resources/area.png"))
+  (def targetCols (.cols diamond))
+  (def targetRows (.rows diamond))
+  (def cols (+ (- (.cols sourceImg) targetCols) 1))
+  (def rows (+ (- (.rows sourceImg) targetRows) 1))
   (def result (Mat.))
   (.create result rows, cols, CvType/CV_32FC1)
   (def displayImg (Mat.))
@@ -27,14 +29,39 @@
   (Imgproc/matchTemplate sourceImg diamond result Imgproc/TM_CCOEFF_NORMED)
   ;(Imgproc/matchTemplate sourceImg diamond result Imgproc/TM_CCORR_NORMED)
   (def threshold 0.8)
-
   (Core/normalize result result 0 255 Core/NORM_MINMAX -1 (Mat.))
-  (Highgui/imwrite "resources/output.png" result)
+  ;(Highgui/imwrite "resources/output.png" result)
 
+  (def floatArr (make-array Float/TYPE (.total result)))
+  (.get result 0 0 floatArr)
+
+  (def derp
+    (reduce 
+    (fn [acc x]
+      {:max (max (:max acc) x)
+        :idx (cond
+               (> x (acc :max)) (acc :currIdx)
+               :else (acc :idx))
+        :currIdx (+ (acc :currIdx) 1)
+        })
+    {:max 0 :idx 0 :currIdx 0}
+    floatArr))
+
+  (def row (Math/floor (/ (:idx derp) cols)))
+  (def col (mod (:idx derp) cols))
+
+  (Core/rectangle displayImg (Point. col row) (Point. (+ col targetCols) (+ row targetRows)) (Scalar. 0 0 255))
+  (Core/rectangle result (Point. col row) (Point. (+ col targetCols) (+ row targetRows)) (Scalar. 0 0 255))
+  (Highgui/imwrite "resources/output.png" displayImg)
+  (Highgui/imwrite "resources/output2.png" result)
+
+
+
+  (get floatArr 11923)
   ;(let [robo (Robot.)]
   ;  (.mousePress robo InputEvent/BUTTON1_MASK)
   ;  (.mouseMove robo 1600 100)
   ;  (.mouseRelease robo InputEvent/BUTTON1_MASK)
   ;  ))
 
-(-main)
+)
