ab6a363535473da79bfe81b6859ec8a9c8b2f246
Teemu Sarapisto <tsarpf@gmail.com>
2016-02-24 03:23:03 +0200

    Works... but reports things flipped over x/y axis

--- src/bejeweled_bot/solver.clj
+++ src/bejeweled_bot/solver.clj
@@ -1,29 +1,91 @@
 (ns bejeweled-bot.solver)
-(use 'bejeweled-bot.gems)
+
+(defn getFirst
+  [gem-array]
+  (first (drop-while #(= nil %) (solve gem-array))))
 
 (defn solve
   "puikula"
   [gem-array]
   (map-indexed
-  (fn [colIdx col]
-    (map-indexed
-      (fn [rowIdx item]
-        (check-matches colIdx rowIdx item))
-      col))
-  gem-array))
+    (fn [colIdx col]
+      (first 
+        (drop-while 
+          #(= nil %)
+          (map-indexed
+            (fn [rowIdx item]
+              (check-matches colIdx rowIdx item gem-array))
+            col))))
+    gem-array))
 
-(defn check-matches [x y gem-type]
-  ())
 
+(defn check-matches 
+  "Check for matches from: 1. this left, 2. left to this, 3. this down, 4. down to this"
+  [x y gem-type gem-array]
+  (let 
+    [right-x (+ x 1)
+     down-y (+ y 1)
+     right-gem-type (get-in gem-array [(+ x 1) y])
+     down-gem-type (get-in gem-array [x (+ y 1)])
+     right-swap (assoc-in 
+                     (assoc-in gem-array [(+ x 1) y] gem-type)
+                     [x y] right-gem-type)
+     down-swap (assoc-in 
+                     (assoc-in gem-array [x (+ y 1)] gem-type)
+                     [x y] down-gem-type)]
+    (cond
+      (would-match right-x y gem-type right-swap) {:x x :y y :dir "right" :debug 1 :gemtype gem-type}
+      (would-match x down-y gem-type down-swap) {:x x :y y :dir "down" :debug 2 :gemtype gem-type}
+      (would-match x y right-gem-type right-swap) {:x x :y y :dir "right" :debug 3 :gemtype right-gem-type}
+      (would-match x y down-gem-type down-swap) {:x x :y y :dir "down" :debug 4 :gemtype down-gem-type}
+      :else nil)))
+
+
+(def x 0)
+(def y 1)
+(def down-y (+ y 1))
+(def gem-type :opal)
+(def down-gem-type (get-in testArray [x (+ y 1)]))
+(def swapd (assoc-in (assoc-in testArray [x (+ y 1)] gem-type) [x y] down-gem-type))
+(def down-swap swapd)
+(def gem-array swapd)
+(would-match x down-y gem-type down-swap) {:x x :y y :dir "down" :debug 2 :gemtype gem-type}
+(defn would-match [x y gem-type gem-array]
+  (and (not (= gem-type nil))
+  (or
+    (= 
+      (get-in gem-array [(+ x 1) y])
+      (get-in gem-array [(+ x 2) y])
+      gem-type)
+    (= 
+      (get-in gem-array [(- x 1) y])
+      (get-in gem-array [(- x 2) y])
+      gem-type)
+    (= 
+      (get-in gem-array [(- x 1) y])
+      (get-in gem-array [(+ x 1) y])
+      gem-type)
+    (= 
+      (get-in gem-array [x (+ y 1)])
+      (get-in gem-array [x (+ y 2)])
+      gem-type)
+    (= 
+      (get-in gem-array [x (- y 1)])
+      (get-in gem-array [x (- y 2)])
+      gem-type)
+    (= 
+      (get-in gem-array [x (- y 1)])
+      (get-in gem-array [x (+ y 1)])
+      gem-type))))
 
 (def testArray
-  [[:ruby        :opal     :ruby       :diamond    :topaz    :amethyst   :sapphire :sapphire]
+  [[:ruby        :opal     :ruby       :ruby :topaz    :amethyst   :sapphire :sapphire]
    [:ruby        :diamond  :amethyst   :diamond    :opal     :smaragdine :sapphire :smaragdine]
-   [:topaz       :sapphire :smaragdine :topaz      :opal     :diamond    :amethyst :ruby]
+   [:topaz       :diamond  :smaragdine :topaz      :opal     :diamond    :amethyst :ruby]
    [:sapphire    :opal     :sapphire   :topaz      :sapphire :smaragdine :ruby     :ruby]
    [:amethyst    :opal     :diamond    :sapphire   :amethyst :ruby       :opal     :opal]
-   [:topaz       :opal     :amethyst   :smaragdine :amethyst :topaz      :sapphire :amethyst]
-   [:smaragdine  :opal     :opal       :opal       :sapphire :ruby       :amethyst :sapphire]
-   [:amethyst    :opal     :diamond    :amethyst   :diamond  :topaz      :opal     :opal]])
+   [:topaz       :amethyst :amethyst   :smaragdine :amethyst :topaz      :sapphire :amethyst]
+   [:smaragdine  :topaz    :opal       :opal       :sapphire :ruby       :amethyst :sapphire]
+   [:amethyst    :sapphire :diamond    :amethyst   :diamond  :topaz      :opal     :opal]])
 
-(solve testArray)
+(getFirst testArray)
